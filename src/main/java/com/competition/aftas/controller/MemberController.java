9325f8b37e6fe81fb72f4f4e1094a783d3955b32
jesakim <jesakim1@gmail.com>
2023-12-20 09:38:22 +0100

    modifing members controller

--- src/main/java/com/competition/aftas/controller/MemberController.java
+++ src/main/java/com/competition/aftas/controller/MemberController.java
@@ -29,7 +29,7 @@ public class MemberController {
     public MemberDTO updateMember(@PathVariable Integer num, @RequestBody MemberDTO updatedMemberDTO) {
         return memberService.updateMember(num, updatedMemberDTO);
     }
-    @DeleteMapping("/{nu")
+    @DeleteMapping("/{num}")
     public void deleteMember(@PathVariable Integer num) {
         memberService.deleteMember(num);
     }
